ba504403b496467032c7dd5b3dbfce446cd943d9
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-07-08 22:29:17 -0300

    Update deliveryOptions.js

--- javascript-amazon-project-exercises-15/data/deliveryOptions.js
+++ javascript-amazon-project-exercises-15/data/deliveryOptions.js
@@ -27,13 +27,20 @@ export function getDeliveryOption(deliveryOptionId) {
 }
 
 export function calculateDeliveryDate(deliveryOption) {
-  const today = dayjs();
-  const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+  let remainingDays = deliveryOption.deliveryDays;
+  let deliveryDate = dayjs();
+
+  while (remainingDays > 0) {
+    deliveryDate = deliveryDate.add(1, 'day');
+    if (!isWeekend(deliveryDate)) {
+      remainingDays--;
+    }
+  }
   const dateString = deliveryDate.format('dddd, MMMM D');
   return dateString;
 }
 
-export function deliveryDateNoSatSun() {
-  let i = 0;
-  while ()
+function isWeekend(date) {
+  const dayOfWeek = date.format('dddd');
+  return dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday';
 }
