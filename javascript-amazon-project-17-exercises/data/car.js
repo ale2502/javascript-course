8d05811b1680e60ab7738014ac87e19050f741f2
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-07-17 16:27:27 -0300

    Exercises 17

--- javascript-amazon-project-17-exercises/data/car.js
+++ javascript-amazon-project-17-exercises/data/car.js
@@ -1,24 +1,27 @@
 class Car {
-  brand;
-  model;
+  #brand;
+  #model;
   speed = 0;
-  isTrunkOpen;
+  isTrunkOpen = false;
 
   constructor(carDetails) {
-    this.brand = carDetails.brand;
-    this.model = carDetails.model;
+    this.#brand = carDetails.brand;
+    this.#model = carDetails.model;
   }
 
   displayInfo() {
-    console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h, is trunk opened? ${this.isTrunkOpen}`);
+    const trunkStatus = this.isTrunkOpen ? 'open' : 'closed';
+
+    console.log(`${this.#brand} ${this.#model}, Speed: ${this.speed} km/h, Trunk: ${trunkStatus}`);
   }
 
   go() {
     this.speed += 5;
-    if (this.speed > 200) {
-      this.speed = 200;
+    if (!this.isTrunkOpen) {
+      this.speed += 5;
     }
-    if (this.isTrunkOpen === false) {
+
+    if (this.speed > 200) {
       this.speed = 0;
     }
   }
@@ -31,14 +34,38 @@ class Car {
   }
 
   openTrunk() {
-    this.isTrunkOpen = true;
-    if (this.speed > 0) {
-      this.isTrunkOpen = false;
+    if (this.speed === 0) {
+      this.isTrunkOpen = true;
     }
   }
 
   closeTrunk() {
     this.isTrunkOpen = false;
+  }
+}
+
+class RaceCar extends Car {
+  acceleration;
+
+  constructor(carDetails) {
+    super(carDetails);
+    this.acceleration = carDetails.acceleration;
+  }
+
+  go() {
+    this.speed += this.acceleration;
+
+    if (this.speed > 300) {
+      this.speed = 300;
+    }
+  }
+
+  openTrunk() {
+    console.log('Race cars do not have a trunk.');
+  }
+
+  closeTrunk() {
+    console.log('Race cars do not have a trunk.');
   }
 }
 
@@ -52,23 +79,17 @@ const car2 = new Car({
   model: 'Model 3'
 });
 
+const raceCar = new RaceCar({
+  brand: 'McLaren',
+  model: 'F1',
+  acceleration: 20
+});
+
 console.log(car1);
 console.log(car2);
 
-car1.displayInfo();
-car2.displayInfo();
+raceCar.go();
+raceCar.go();
+raceCar.displayInfo();
 car1.go();
-car1.go();
-car1.go();
-car1.go();
-car1.break();
 car1.displayInfo();
-car1.openTrunk();
-car1.displayInfo();
-car1.break();
-car1.break();
-car1.break();
-car1.displayInfo();
-car1.openTrunk();
-car1.displayInfo();
-car1.go();
