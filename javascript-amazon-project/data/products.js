9a761efc3c8b6647e347b707a95551087ba1c05b
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-07-21 11:59:44 -0300

    Learn error handling

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -104,6 +104,8 @@ export function loadProductsFetch() {
       return new Product(productDetails);
     });
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
@@ -130,10 +132,13 @@ export function loadProducts(fun) {
     fun();
   });
 
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
-
 
 /*
 export const products = [
