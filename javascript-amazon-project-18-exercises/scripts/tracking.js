5b7446ff61b8352d2b786df78941976ab94b5709
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-07-25 22:33:03 -0300

    Studying

--- javascript-amazon-project-18-exercises/scripts/tracking.js
+++ javascript-amazon-project-18-exercises/scripts/tracking.js
@@ -1,39 +1,46 @@
 import { orders } from "../data/orders.js";
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
 function loadTrackingPage() {
-    const trackingHTML = `
-        <a class="back-to-orders-link link-primary" href="orders.html">
-          View all orders
-        </a>
+  const urlParams = new URLSearchParams(window.location.search);
+  const productId = urlParams.get('orderID');
+  const deliveryTimeString = dayjs()
 
-        <div class="delivery-date">
-          Arriving on Monday, June 13
-        </div>
+  const trackingHTML = `
+    <a class="back-to-orders-link link-primary" href="orders.html">
+      View all orders
+    </a>
 
-        <div class="product-info">
-          Black and Gray Athletic Cotton Socks - 6 Pairs
-        </div>
+    <div class="delivery-date">
+      Arriving on Monday, June 13
+    </div>
 
-        <div class="product-info">
-          Quantity: 1
-        </div>
+    <div class="product-info">
+      Black and Gray Athletic Cotton Socks - 6 Pairs
+    </div>
 
-        <img class="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg">
+    <div class="product-info">
+      Quantity: 1
+    </div>
 
-        <div class="progress-labels-container">
-          <div class="progress-label">
-            Preparing
-          </div>
-          <div class="progress-label current-status">
-            Shipped
-          </div>
-          <div class="progress-label">
-            Delivered
-          </div>
-        </div>
+    <img class="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg">
 
-        <div class="progress-bar-container">
-          <div class="progress-bar"></div>
-        </div>
-    `;
+    <div class="progress-labels-container">
+      <div class="progress-label">
+        Preparing
+      </div>
+      <div class="progress-label current-status">
+        Shipped
+      </div>
+      <div class="progress-label">
+        Delivered
+      </div>
+    </div>
+
+    <div class="progress-bar-container">
+      <div class="progress-bar"></div>
+    </div>
+  `;
 }
+const urlParams = new URLSearchParams(window.location.search);
+console.log(urlParams.get('orderId'));
